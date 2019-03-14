3c0f638d4c638fdda0cb8990d83ca55998c0b863
Sandra Moen <Slideshow776@users.noreply.github.com>
2019-03-14 13:23:26 +0100

    Update SharkyTest.kt

--- chapter2/tests/SharkyTest.kt
+++ chapter2/tests/SharkyTest.kt
@@ -11,7 +11,7 @@ class SharkyTest {
     @Test
     fun `testing if positions is within boundaries`() {
         sharky.setPosition(100.toFloat(), 100.toFloat())
-        for (i in 0..1_000_000) { // 1*10^6 iterations amounts to about (1*10^6)/60 = 4.63 hours
+        for (i in 0..1_000_000) { // 1*10^6 iterations amounts to about [(1*10^6)/60]/60 ~= 278 hours
             sharky.act(i.toFloat())
             assertTrue(sharky.x in 50.0..750.0)
             assertTrue(sharky.y in 50.0..550.0)

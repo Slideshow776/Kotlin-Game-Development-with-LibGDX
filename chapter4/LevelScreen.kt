5159d9efd01ce096e4c35008a24ad36241123a20
Sandra Moen <sandramoen01@gmail.com>
2019-04-30 07:21:23 +0200

    added rocks, explosions and respective logic to go along with that

--- chapter4/LevelScreen.kt
+++ chapter4/LevelScreen.kt
@@ -14,9 +14,42 @@ class LevelScreen : BaseScreen() {
         BaseActor.setWorldBounds(space)
 
         spaceship = Spaceship(400f, 300f, mainStage)
+
+        Rock(600f, 500f, mainStage)
+        Rock(600f, 300f, mainStage)
+        Rock(600f, 100f, mainStage)
+        Rock(400f, 100f, mainStage)
+        Rock(200f, 100f, mainStage)
+        Rock(200f, 300f, mainStage)
+        Rock(200f, 500f, mainStage)
+        Rock(400f, 500f, mainStage)
     }
 
-    override fun update(dt: Float) {}
+    override fun update(dt: Float) {
+        for (rockActor: BaseActor in BaseActor.getList(mainStage, Rock::class.java.canonicalName)) {
+            if (rockActor.overlaps(spaceship)) {
+                if (spaceship.shieldPower <= 0) {
+                    var boom = Explosion(0f, 0f, mainStage)
+                    boom.centerAtActor(spaceship)
+                    spaceship.remove()
+                    spaceship.setPosition(-1000f, -1000f)
+                } else {
+                    spaceship.shieldPower -=34
+                    var boom = Explosion(0f, 0f, mainStage)
+                    boom.centerAtActor(rockActor)
+                    rockActor.remove()
+                }
+            }
+            for (laserActor: BaseActor in BaseActor.getList(mainStage, Laser::class.java.canonicalName)) {
+                if (laserActor.overlaps(rockActor)) {
+                    var boom = Explosion(0f, 0f, mainStage)
+                    boom.centerAtActor(rockActor)
+                    laserActor.remove()
+                    rockActor.remove()
+                }
+            }
+        }
+    }
 
     // Override default InputProcessor method
     override fun keyDown(keycode: Int): Boolean {
---
+++ chapter4/Rock.kt
@@ -0,0 +1,27 @@
+package chapter4
+
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
+
+class Rock(x: Float, y: Float, s: Stage) : BaseActor(x, y, s) {
+    init {
+        loadTexture("assets/rock.png")
+
+        var random = MathUtils.random(30f)
+
+        addAction(Actions.forever(Actions.rotateBy(30f + random, 1f)))
+
+        setSpeed(50f + random)
+        setSpeed(50f + random)
+        setDeceleration(0f)
+
+        setMotionAngle(MathUtils.random(360f))
+    }
+
+    override fun act(dt: Float) {
+        super.act(dt)
+        applyPhysics(dt)
+        wrapArpundWorld()
+    }
+}

cdbe0c949869af151d8bdb0a33a13d8805f45f92
Sandra Moen <sandramoen01@gmail.com>
2019-06-04 06:30:59 +0200

    Menu and story screen added

--- chapter5/theMissingHomework/HomeworkGame.kt
+++ chapter5/theMissingHomework/HomeworkGame.kt
@@ -1,16 +1,3 @@
-/*
-package chapter5.theMissingHomework
-
-import chapter5.theMissingHomework.BaseGame
-
-class HomeworkGame: BaseGame() {
-    override fun create() {
-        super.create()
-        setActiveScreen(MenuScreen() as BaseScreen)
-    }
-}
-*/
-
 package chapter5.theMissingHomework
 
 class HomeworkGame: BaseGame() {
---
+++ chapter5/theMissingHomework/StoryScreen.kt
@@ -0,0 +1,18 @@
+package chapter5.theMissingHomework
+
+class StoryScreen : BaseScreen() {
+
+    override fun initialize() {}
+
+    fun hallway() {}
+
+    fun classroom() {}
+
+    fun scienceLab() {}
+
+    fun library() {}
+
+    override fun update(dt: Float) {}
+
+    override fun keyDown(keycode: Int): Boolean { return false }
+}

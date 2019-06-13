0bf1d6cdd8114c01c666c1fa8089ffc17f665eb1
Sandra Moen <sandramoen01@gmail.com>
2019-06-13 07:51:23 +0200

    synchronizing game objects and audio

--- chapter6/rhythmTapper/RhythmScreen.kt
+++ chapter6/rhythmTapper/RhythmScreen.kt
@@ -1,7 +1,11 @@
 package chapter6.rhythmTapper
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.audio.Music
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.scenes.scene2d.Event
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import java.util.ArrayList
 import java.util.Collections
 
@@ -10,6 +14,13 @@ class RhythmScreen: BaseScreen() {
     private lateinit var colorList: ArrayList<Color>
     private lateinit var targetList: ArrayList<TargetBox>
     private lateinit var fallingList: ArrayList<ArrayList<FallingBox>>
+
+    private lateinit var gameMusic: Music
+    private var songData: SongData? = null
+    private val leadTime = 3f
+    private var advanceTimer = 0f
+    private var spawnHeight = 650f
+    private var noteSpeed = (spawnHeight - targetList.get(0).getY()) / leadTime
 
     override fun initialize() {
         val background = BaseActor(0f, 0f, mainStage)
@@ -42,8 +53,56 @@ class RhythmScreen: BaseScreen() {
         for (i in 0..3) {
             fallingList.add(ArrayList<FallingBox>())
         }
+
+        advanceTimer = 0f
+        spawnHeight = 650f
+        noteSpeed = (spawnHeight - targetList.get(0).getY()) / leadTime
+
+        val startButton = TextButton("Start", BaseGame.textButtonStyle)
+        startButton.addListener { e: Event ->
+            if (isTouchDownEvent(e)) {
+                val dataFileHandle = Gdx.files.internal("assets/FunkyJunky.key") // hard coded path
+                songData = SongData()
+                songData!!.readFromFile(dataFileHandle)
+                songData!!.resetIndex()
+
+                val songFileHandle = Gdx.files.internal("assets/" + songData!!.songName)
+                gameMusic = Gdx.audio.newMusic(songFileHandle)
+                startButton.isVisible = false
+            }
+            false
+        }
+
+        uiTable.add(startButton)
     }
 
-    override fun update(dt: Float) {}
+    override fun update(dt: Float) {
+        if (songData == null)
+            return
+
+        if (advanceTimer < leadTime && advanceTimer + dt > leadTime)
+            gameMusic.play()
+
+        if (advanceTimer < leadTime)
+            advanceTimer += dt
+        else
+            advanceTimer = leadTime + gameMusic.position
+
+        while (!songData!!.isFinished && advanceTimer >= songData!!.currentKeyTime.time!!) {
+            val key = songData!!.currentKeyTime.key
+            val i = keyList.indexOf(key)
+
+            val fb = FallingBox(targetList.get(i).getX(), spawnHeight, mainStage)
+            fb.setSpeed(noteSpeed)
+            fb.setMotionAngle(270f)
+            /*fb.setColor(colorList.get(i))*/
+            fb.color = colorList[i]
+
+            fallingList.get(i).add(fb)
+
+            songData!!.advanceIndex()
+        }
+    }
+
     override fun keyDown(keycode: Int): Boolean { return false }
 }

ed940824f4d5a36e6fa72faecf513012e8425c90
Sandra Moen <sandramoen01@gmail.com>
2019-07-09 12:07:02 +0200

    Added paddle

--- chapter8/LevelScreen.kt
+++ chapter8/LevelScreen.kt
@@ -1,11 +1,20 @@
 package chapter8
 
-class LevelScreen : BaseScreen() {
-    override fun initialize() {
+import com.badlogic.gdx.Gdx
 
+class LevelScreen : BaseScreen() {
+    private lateinit var paddle: Paddle
+    override fun initialize() {
+        val background = BaseActor(0f, 0f, mainStage)
+        background.loadTexture("assets/space.png")
+        BaseActor.setWorldBounds(background)
+
+        paddle = Paddle(320f, 32f, mainStage)
+        paddle.boundToWorld()
     }
 
     override fun update(dt: Float) {
-
+        val mouseX = Gdx.input.x
+        paddle.x = mouseX - paddle.width/2
     }
 }

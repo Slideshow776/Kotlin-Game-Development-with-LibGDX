dbd0d9d34f797ffb02ee48c184e81b2f9dd5c6f2
Sandra Moen <sandramoen01@gmail.com>
2019-07-09 12:32:09 +0200

    Added bricks

---
+++ chapter8/Brick.kt
@@ -0,0 +1,9 @@
+package chapter8
+
+import com.badlogic.gdx.scenes.scene2d.Stage
+
+class Brick(x: Float, y: Float, s: Stage) : BaseActor(x, y, s) {
+    init {
+        loadTexture("""assets/brick-gray.png""")
+    }
+}
--- chapter8/LevelScreen.kt
+++ chapter8/LevelScreen.kt
@@ -14,6 +14,24 @@ class LevelScreen : BaseScreen() {
         Wall(0f, 0f, mainStage, 20f, 600f) // left wall
         Wall(780f, 0f, mainStage, 20f, 600f) // right wall
         Wall(0f, 550f, mainStage, 800f, 50f) // top wall
+
+        val tempBrick = Brick(0f, 0f, mainStage)
+        val brickWidth = tempBrick.width
+        val brickHeight = tempBrick.height
+        tempBrick.remove()
+
+        val totalRows = 10
+        val totalCols = 10
+        val marginX = (800 - totalCols * brickWidth) / 2
+        val marginY = (600 - totalRows * brickHeight) -120
+
+        for (rowNum in 0 until totalRows) {
+            for (colNum in 0 until totalCols) {
+                val x = marginX + brickWidth * colNum
+                val y = marginY + brickHeight * rowNum
+                Brick(x, y, mainStage)
+            }
+        }
     }
 
     override fun update(dt: Float) {

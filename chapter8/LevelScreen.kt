857ee2bd6a3bbbe1ad3e0b481e1295bc2cad57b4
Sandra Moen <sandramoen01@gmail.com>
2019-07-09 14:39:58 +0200

    User interface added

--- chapter8/LevelScreen.kt
+++ chapter8/LevelScreen.kt
@@ -1,11 +1,18 @@
 package chapter8
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 
 class LevelScreen : BaseScreen() {
     private lateinit var paddle: Paddle
     private lateinit var ball: Ball
+    private var score = 0
+    private var balls = 3
+    private lateinit var scoreLabel: Label
+    private lateinit var ballsLabel: Label
+    private lateinit var messageLabel: Label
 
     override fun initialize() {
         // background
@@ -42,6 +49,19 @@ class LevelScreen : BaseScreen() {
 
         // ball
         ball = Ball(0f, 0f, mainStage)
+
+        // game
+        scoreLabel = Label("Score: $score", BaseGame.labelStyle)
+        ballsLabel = Label("Balls: $balls", BaseGame.labelStyle)
+        messageLabel = Label("Click to start", BaseGame.labelStyle)
+        messageLabel.color = Color.CYAN
+
+        uiTable.pad(5f)
+        uiTable.add(scoreLabel)
+        uiTable.add().expandX()
+        uiTable.add(ballsLabel)
+        uiTable.row()
+        uiTable.add(messageLabel).colspan(3).expandY()
     }
 
     override fun update(dt: Float) {
@@ -64,6 +84,8 @@ class LevelScreen : BaseScreen() {
             if (ball.overlaps(brick)) {
                 ball.bounceOff(brick)
                 brick.remove()
+                score += 100
+                scoreLabel.setText(("Score: $score"))
             }
         }
 
@@ -73,11 +95,36 @@ class LevelScreen : BaseScreen() {
             val bounceAngle = MathUtils.lerp(150f, 30f, paddlePercentHit)
             ball.setMotionAngle(bounceAngle)
         }
+
+        if (BaseActor.count(mainStage, Brick::class.java.canonicalName) == 0) {
+            messageLabel.setText("You win!")
+            messageLabel.color = Color.LIME
+            messageLabel.isVisible = true
+        }
+
+        if (ball.y < -50 && BaseActor.count(mainStage, Brick::class.java.canonicalName) > 0) {
+            ball.remove()
+
+            if (balls > 0) {
+                balls -= 1
+                ballsLabel.setText("Balls: " + balls)
+                ball = Ball(0f, 0f, mainStage)
+
+                messageLabel.setText("Click to start")
+                messageLabel.color = Color.CYAN
+                messageLabel.isVisible = true
+            } else {
+                messageLabel.setText("Game Over")
+                messageLabel.color = Color.RED
+                messageLabel.isVisible = true
+            }
+        }
     }
 
     override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
         if (ball.isPaused()) {
             ball.setPaused(false)
+            messageLabel.isVisible = false
         }
         return false
     }

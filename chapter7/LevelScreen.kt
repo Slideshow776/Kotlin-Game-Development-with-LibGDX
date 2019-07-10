411cf61d281fa21afa20a624b888b2d2dd6efa18
Sandra Moen <sandramoen01@gmail.com>
2019-07-10 12:40:42 +0200

    Added infinite scrolling

--- chapter7/LevelScreen.kt
+++ chapter7/LevelScreen.kt
@@ -2,7 +2,10 @@ package chapter7
 
 class LevelScreen : BaseScreen() {
     override fun initialize() {
-
+        Sky(0f, 0f, mainStage)
+        Sky(800f, 0f, mainStage)
+        Ground(0f, 0f, mainStage)
+        Ground(800f, 0f, mainStage)
     }
 
     override fun update(dt: Float) {

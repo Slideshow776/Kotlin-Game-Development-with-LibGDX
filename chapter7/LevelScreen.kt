6d3db87fde1fb71f9b51fda7fd3c758ec1b0ef0e
Sandra Moen <sandramoen01@gmail.com>
2019-07-10 13:22:36 +0200

    Added stars

--- chapter7/LevelScreen.kt
+++ chapter7/LevelScreen.kt
@@ -1,9 +1,15 @@
 package chapter7
 
 import com.badlogic.gdx.Input.Keys
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 
 class LevelScreen : BaseScreen() {
     private lateinit var plane: Plane
+    private var starTimer = 0f
+    private var starSpawnInterval = 4f
+    private var score = 0
+    private lateinit var scoreLabel: Label
 
     override fun initialize() {
         Sky(0f, 0f, mainStage)
@@ -13,9 +19,30 @@ class LevelScreen : BaseScreen() {
 
         plane = Plane(100f, 500f, mainStage)
         BaseActor.setWorldBounds(800f, 600f)
+
+        scoreLabel = Label("$score", BaseGame.labelStyle)
+
+        uiTable.pad(10f)
+        uiTable.add(scoreLabel)
+        uiTable.row()
+        uiTable.add().expandY()
     }
 
-    override fun update(dt: Float) {}
+    override fun update(dt: Float) {
+        starTimer += dt
+        if (starTimer > starSpawnInterval) {
+            Stars(800f, MathUtils.random(100f, 500f), mainStage)
+            starTimer = 0f
+        }
+
+        for (star: BaseActor in BaseActor.getList(mainStage, Stars::class.java.canonicalName)) {
+            if (plane.overlaps(star)) {
+                star.remove()
+                score++
+                scoreLabel.setText("$score")
+            }
+        }
+    }
 
     override fun keyDown(keycode: Int): Boolean {
         if (keycode == Keys.SPACE) {

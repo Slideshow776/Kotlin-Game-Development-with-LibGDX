2f189b91c155b2571d7ec485368cfce0ebbb8fef
Sandra Moen <sandramoen01@gmail.com>
2019-07-16 07:02:16 +0200

    Adde restart button

--- chapter7/LevelScreen.kt
+++ chapter7/LevelScreen.kt
@@ -5,7 +5,10 @@ import com.badlogic.gdx.Input.Keys
 import com.badlogic.gdx.audio.Music
 import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.scenes.scene2d.Event
 import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 
 class LevelScreen : BaseScreen() {
     private lateinit var plane: Plane
@@ -27,6 +30,8 @@ class LevelScreen : BaseScreen() {
     private lateinit var sparkleSound: Sound
     private lateinit var explosionSound: Sound
 
+    private lateinit var restartButton: TextButton
+
     override fun initialize() {
         Sky(0f, 0f, mainStage)
         Sky(800f, 0f, mainStage)
@@ -42,10 +47,24 @@ class LevelScreen : BaseScreen() {
         gameOverMessage.loadTexture("assets/game-over.png")
         gameOverMessage.isVisible = false
 
+        restartButton = TextButton("Restart", BaseGame.textButtonStyle)
+        restartButton.addListener {e: Event ->
+            if (isTouchDownEvent(e))
+                BaseGame.setActiveScreen(MenuScreen())
+            false
+        }
+        restartButton.isVisible = false
+
+        // scene graph
+        val messageTable = Table()
+        messageTable.add(gameOverMessage)
+        messageTable.row()
+        messageTable.add(restartButton)
+
         uiTable.pad(10f)
-        uiTable.add(scoreLabel)
+        uiTable.add(scoreLabel).top()
         uiTable.row()
-        uiTable.add(gameOverMessage).expandY()
+        uiTable.add(messageTable).expandY()
 
         sparkleSound = Gdx.audio.newSound(Gdx.files.internal("assets/sparkle.mp3"))
         explosionSound = Gdx.audio.newSound(Gdx.files.internal("assets/explosion.wav"))
@@ -104,6 +123,7 @@ class LevelScreen : BaseScreen() {
                     backgroundMusic.stop()
                     plane.remove()
                     gameOver = true
+                    restartButton.isVisible = true
                     BaseGame.writeHighScore(score)
                     gameOverMessage.isVisible = true
                     for (enemy1: BaseActor in BaseActor.getList(mainStage, Enemy::class.java.canonicalName)) {

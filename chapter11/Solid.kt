19e35ff6fd12f48666166066392e9e781eb27eea
Sandra Moen <sandramoen01@gmail.com>
2019-07-30 07:26:02 +0200

    Starting the Level

---
+++ chapter11/Solid.kt
@@ -0,0 +1,12 @@
+package chapter11
+
+import com.badlogic.gdx.scenes.scene2d.Stage
+
+class Solid(x: Float, y: Float, width: Float, height: Float, s: Stage): BaseActor(x, y, s) {
+    var enabled = true
+
+    init {
+        setSize(width, height)
+        setBoundaryRectangle()
+    }
+}

42376424f2bd05a821e57f727b830a960be00ab5
Sandra Moen <sandramoen01@gmail.com>
2019-08-01 06:51:14 +0200

    User interface

--- chapter11/LevelScreen.kt
+++ chapter11/LevelScreen.kt
@@ -2,9 +2,20 @@ package chapter11
 
 import kotlin.math.abs
 import com.badlogic.gdx.Input.Keys
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Table
 
 class LevelScreen : BaseScreen() {
     private lateinit var jack: Koala
+
+    private var gameOver = false
+    private var coins = 0
+    private var time = 60f
+    private lateinit var coinLabel: Label
+    private lateinit var keyTable: Table
+    private lateinit var timeLabel: Label
+    private lateinit var messageLabel: Label
 
     override fun initialize() {
         val tma = TilemapActor("assets/map.tmx", mainStage)
@@ -23,6 +34,21 @@ class LevelScreen : BaseScreen() {
         val startPoint = tma.getRectangleList("start")[0]
         val startProps = startPoint.properties
         jack = Koala(startProps.get("x") as Float, startProps.get("y") as Float, mainStage)
+
+        coinLabel = Label("Coins: $coins", BaseGame.labelStyle)
+        coinLabel.color = Color.GOLD
+        keyTable = Table()
+        timeLabel = Label("Time: ${time.toInt()}", BaseGame.labelStyle)
+        timeLabel.color = Color.LIGHT_GRAY
+        messageLabel = Label("Message", BaseGame.labelStyle)
+        messageLabel.isVisible = false
+
+        uiTable.pad(20f)
+        uiTable.add(coinLabel)
+        uiTable.add(keyTable).expandX()
+        uiTable.add(timeLabel)
+        uiTable.add().row()
+        uiTable.add(messageLabel).colspan(3).expandY()
     }
 
     override fun update(dt: Float) {

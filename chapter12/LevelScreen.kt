f0545c92bc95e1e1a67df2f18f8e2d94e00bbfa6
Sandra Moen <sandramoen01@gmail.com>
2019-08-08 07:11:22 +0200

    Level Setup

--- chapter12/LevelScreen.kt
+++ chapter12/LevelScreen.kt
@@ -2,7 +2,18 @@ package chapter12
 
 class LevelScreen : BaseScreen() {
     override fun initialize() {
+        val tma = TilemapActor("assets/map.tmx", mainStage)
 
+        for (obj in tma.getRectangleList("solid")) {
+            val props = obj.properties
+            Solid(
+                props.get("x") as Float,
+                props.get("y") as Float,
+                props.get("width") as Float,
+                props.get("height") as Float,
+                mainStage
+            )
+        }
     }
 
     override fun update(dt: Float) {

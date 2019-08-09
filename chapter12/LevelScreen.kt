f8d7fd38c0ff08c5f2eed5fba7fd112a6074e332
Sandra Moen <sandramoen01@gmail.com>
2019-08-09 06:53:31 +0200

    Added the sword

--- chapter12/LevelScreen.kt
+++ chapter12/LevelScreen.kt
@@ -2,9 +2,12 @@ package chapter12
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input.Keys
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
 
 class LevelScreen : BaseScreen() {
     lateinit var hero: Hero
+    lateinit var sword: Sword
 
     override fun initialize() {
         val tma = TilemapActor("assets/map.tmx", mainStage)
@@ -23,21 +26,68 @@ class LevelScreen : BaseScreen() {
         val startPoint = tma.getRectangleList("start")[0]
         val startProps = startPoint.properties
         hero = Hero(startProps.get("x") as Float, startProps.get("y") as Float, mainStage)
+
+        sword = Sword(0f, 0f, mainStage)
+        sword.isVisible = false
     }
 
     override fun update(dt: Float) {
-        // hero movement controls
-        if (Gdx.input.isKeyPressed(Keys.LEFT))
-            hero.accelerateAtAngle(180f)
-        if (Gdx.input.isKeyPressed(Keys.RIGHT))
-            hero.accelerateAtAngle(0f)
-        if (Gdx.input.isKeyPressed(Keys.UP))
-            hero.accelerateAtAngle(90f)
-        if (Gdx.input.isKeyPressed(Keys.DOWN))
-            hero.accelerateAtAngle(270f)
+
+        if (!sword.isVisible) {
+            // hero movement controls
+            if (Gdx.input.isKeyPressed(Keys.LEFT))
+                hero.accelerateAtAngle(180f)
+            if (Gdx.input.isKeyPressed(Keys.RIGHT))
+                hero.accelerateAtAngle(0f)
+            if (Gdx.input.isKeyPressed(Keys.UP))
+                hero.accelerateAtAngle(90f)
+            if (Gdx.input.isKeyPressed(Keys.DOWN))
+                hero.accelerateAtAngle(270f)
+        }
 
         for (solid in BaseActor.getList(mainStage, Solid::class.java.canonicalName)) {
             hero.preventOverlap(solid)
         }
+    }
+
+    override fun keyDown(keycode: Int): Boolean {
+        if (keycode == Keys.S)
+            swingSword()
+        return false
+    }
+
+    fun swingSword() {
+        // visibility determines if sword is currently swinging
+        if (sword.isVisible)
+            return
+
+        hero.setSpeed(0f)
+
+        val facingAngle = hero.facingAngle
+
+        val offset = Vector2()
+        when (facingAngle) {
+            0f -> offset.set(.5f, .2f)
+            90f -> offset.set(.65f, .5f)
+            180f -> offset.set(.4f, .2f)
+            else -> offset.set(.25f, .2f)
+        }
+
+        sword.setPosition(hero.x, hero.y)
+        sword.moveBy(offset.x * hero.width, offset.y * hero.height)
+
+        val swordArc = 90f
+        sword.rotation = facingAngle - swordArc / 2
+        sword.originX = 0f
+
+        sword.isVisible = true
+        sword.addAction(Actions.rotateBy(swordArc, .25f))
+        sword.addAction(Actions.after(Actions.visible(false)))
+
+        // hero should appear in front of sword when facing north or west
+        if (facingAngle == 90f || facingAngle == 180f)
+            hero.toFront()
+        else
+            sword.toFront()
     }
 }
